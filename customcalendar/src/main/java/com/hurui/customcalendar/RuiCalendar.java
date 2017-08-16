f73c107546e4c513c2238ce4579a6be319ce8677
胡瑞 <hurui@huruideMacBook-Pro.local>
2017-08-16 10:31:57 +0800

    去除一些不必要的log

--- customcalendar/src/main/java/com/hurui/customcalendar/RuiCalendar.java
+++ customcalendar/src/main/java/com/hurui/customcalendar/RuiCalendar.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -129,7 +128,6 @@ public class RuiCalendar extends LinearLayout implements View.OnClickListener, A
         }
         int childCount = mGridView.getChildCount();
         for(int n=0; n< childCount; n++){
-            Log.i("=========", n+"");
             mGridView.getChildAt(n).setBackgroundColor(Color.parseColor("#00E6E4E5"));
         }
         mGridView.getChildAt(i).setBackgroundColor(Color.parseColor("#FFE6E4E5"));
